6bd2ede756a9ce21f38376d97dbd98992d32e829
Ankit <ankitmhatre.mhatre9@gmail.com>
2019-01-16 19:08:44 +0530

    added new functions

--- app/src/main/java/com/am/SimpleSocketActivty.java
+++ app/src/main/java/com/am/SimpleSocketActivty.java
@@ -9,39 +9,60 @@ import com.am.dialer.R;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.PrintWriter;
+import java.net.*;
+import java.util.Enumeration;
 
 public class SimpleSocketActivty extends AppCompatActivity {
     ServerSocket ss = null;
+    Socket s = null;
     boolean end = false;
+    String gloabalres = "";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.simple_for_socket_layout);
+        gloabalres = "HI Ankit here";
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+
+                    Log.d("ipadddr", getLocalAddress().getHostAddress().toString());
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+
         Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
 
                     ss = new ServerSocket(Integer.parseInt(PrefUtils.getString(SimpleSocketActivty.this, "port", "80")));
-                    Socket s;
+
                     Log.d("serversock", "created");
+
 
                     while (!end) {
                         //Server is waiting for client here, if needed
                         s = ss.accept();
-                        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-                        Log.d("socketAnswer", input.readLine());
-                        // PrintWriter output = new PrintWriter(s.getOutputStream());
-                        // s.close();
+                        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                        PrintWriter output = new PrintWriter(s.getOutputStream());
+
+                        receiveMessage(input.readLine());
+                        output.write(sendMessage());
                     }
                     ss.close();
 
@@ -54,5 +75,32 @@ public class SimpleSocketActivty extends AppCompatActivity {
         });
         t.start();
 
+    }
+
+    public void receiveMessage(String res) {
+
+    }
+
+    public String sendMessage() {
+        return gloabalres;
+    }
+
+    private InetAddress getLocalAddress() throws IOException {
+
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        //return inetAddress.getHostAddress().toString();
+                        return inetAddress;
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 }
